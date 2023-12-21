65b935f85d82030a192de4e417df3a3249e585ad
free <hunter.com>
2023-12-21 23:43:31 +0800

    登录界面添加国际化功能

--- cruddemo/src/main/java/com/free/cruddemo/config/MyMvcConfig.java
+++ cruddemo/src/main/java/com/free/cruddemo/config/MyMvcConfig.java
@@ -1,6 +1,9 @@
 package com.free.cruddemo.config;
 
+import com.free.cruddemo.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,5 +19,11 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
         //super.addViewControllers(registry);
         //浏览器发送请求/，来到index页面
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
