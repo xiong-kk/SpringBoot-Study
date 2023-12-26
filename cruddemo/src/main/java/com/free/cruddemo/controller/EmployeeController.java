1f7a225ca67e1241ce926caf89d32130aaf23dcb
free <hunter.com>
2023-12-26 23:03:57 +0800

    添加页面

--- cruddemo/src/main/java/com/free/cruddemo/controller/EmployeeController.java
+++ cruddemo/src/main/java/com/free/cruddemo/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.free.cruddemo.controller;
 
+import com.free.cruddemo.dao.DepartmentDao;
 import com.free.cruddemo.dao.EmployeeDao;
+import com.free.cruddemo.entities.Department;
 import com.free.cruddemo.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,9 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
     //查询所以员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
@@ -24,5 +29,16 @@ public class EmployeeController {
         //将查到的员工信息放到请求域中
         model.addAttribute("emps",employees);
         return "emp/list";
+    }
+
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+
+        //查出所以的部门
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        System.out.println(departments);
+        return "emp/add";
     }
 }
